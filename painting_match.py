7feea16457eebb61020f25545d89824b4a3894ed
Valentin Rublack <valentin.rublack@gmail.com>
2017-01-14 16:18:04 -0700

    Changed stub

--- painting_match.py
+++ painting_match.py
@@ -2,10 +2,10 @@ def match_with_painting(candidate_fname):
     """
 
     :param candidate_fname: Filename of picture that the client uploaded
-    :return:  Photo out of candidate photos that best matches a painting, along with that painting. None if there are no
-    well-matching painings.
+    :return:  Filename of painting that matches the candidate photo well. None if there are no
+    well-matching paintings.
     """
 
     # TODO implement
 
-    return None, None
+    return 'neural-style/sample_style.jpg'
